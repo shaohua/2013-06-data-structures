c8c83af417de97c3e7c3ebdf33ed8d12b2b5780c
Hack Reactor Students <github@hackreactor.com>
2013-06-19 15:07:55 -0700

    adding first working version for queue

--- spec/queueSpec.js
+++ spec/queueSpec.js
@@ -21,5 +21,65 @@ describe("queue", function() {
     expect(queue.size).to.be.a('function');
   });
 
+  it('should take a value at location zero ', function() {
+    queue.enqueue('barrr');
+    expect(queue[0]).to.equal('barrr');
+  });
+
+  it('should remove a value', function() {
+    queue.enqueue('bar');
+    expect( queue.dequeue() ).to.equal('bar');
+  });
+
+  it('should have the correct size after several operations', function() {
+    queue.enqueue('bar1');
+    queue.enqueue('bar2');
+    expect( queue.size() ).to.equal(2);
+    queue.dequeue();
+    expect( queue.size() ).to.equal(1);
+    queue.dequeue();
+    expect( queue.size() ).to.equal(0);
+  });
+
+  it('should delete the enqueued item ', function() {
+    queue.enqueue('bar');
+    queue.dequeue();
+    expect( queue[0] ).to.equal(undefined);
+  });
+
+  it('should handle dequeueing from an empty queue', function() {
+    queue.enqueue('bar');
+    queue.dequeue();
+    expect( queue.dequeue() ).to.equal(undefined);
+  });
+
+  it('should handle dequeue multiple times ', function() {
+    queue.enqueue('bar');
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    queue.dequeue();
+    queue.enqueue('bar');
+    expect( queue.size() ).to.equal(1);
+    // expect( queue[-2] ).to.equal('bar');
+  });
+
+  it('should handle reaaaaally complicated queue activity', function() {
+    // debugger;
+    queue.enqueue('a');
+    queue.enqueue('b');
+    queue.enqueue('c');
+    queue.dequeue();
+    queue.enqueue('d');
+    queue.enqueue('e');
+    queue.dequeue();
+    queue.enqueue('f');
+    // debugger;
+    expect(queue.dequeue()).to.equal('c');
+    expect(queue.dequeue()).to.equal('d');
+    expect(queue.dequeue()).to.equal('e');
+  });
+
+
   // Hey! Add tests here that thoroughly test the functionality of your queue
 });
--- src/functional/queue.js
+++ src/functional/queue.js
@@ -2,18 +2,36 @@ var makeQueue = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size; // Hint: set an initial value here
+  var size = 0; // Hint: set an initial value here
+
+  var head_key = 0;
+  var back_key = 0;
 
   // Implement the methods below
   var queue = {};
 
   queue.enqueue = function(value){
+    size += 1;
+    queue[back_key] = value;
+    back_key += 1;
   };
 
   queue.dequeue = function(){
+    if(size > 0){
+      size -= 1;
+
+      var output = queue[head_key]; //when head_key is 0 and queue is empty, this is undefined
+      delete queue[head_key];
+
+      head_key += 1; //pointing head to the new key
+      return output; //using the old key of the head
+    } else {
+      return undefined;
+    }
   };
 
   queue.size = function(){
+    return size;
   };
 
   return queue;
