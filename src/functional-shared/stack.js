7c945f35a560fc0f01307bcc110756b5fc3724aa
Hack Reactor Students <github@hackreactor.com>
2013-06-19 16:27:39 -0700

    shared function stack.js is not working

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,32 +1,52 @@
+var sharedObj = {};
+
+sharedObj.push = function(value){
+  this[howBig] = value;
+  howBig += 1;
+};
+
+sharedObj.pop = function(){
+  if(howBig >= 0) {
+    howBig -= 1;
+    var temp = this[howBig];
+    delete this[howBig];
+    return temp;
+  } else {
+    return undefined;
+  }
+};
+
+sharedObj.size = function(){
+  return howBig;
+};
+
+var _ = {};
+_.extend = function(obj) {
+
+  var args = Array.prototype.slice.call(arguments);
+  if(args.length === 1){
+    return obj;
+  }
+
+  for(var i=1;i<args.length;i++){
+    for(var key in args[i]){
+      obj[key] = args[i][key];
+    }
+  }
+  return obj;
+};
+
 var makeStack = function(){
   // Use an object with numeric keys to store values
   var storage = {};
 
-  var size = 0;
+  var howBig = 0;
 
   // Implement the methods below
   var stack = {};
 
-  stack.push = function(value){
-    stack[size] = value;
-    size += 1;
-  };
-
-  stack.pop = function(){
-    if(size >= 0) {
-      size -= 1;
-      var temp = stack[size];
-      delete stack[size];
-      return temp;
-    } else {
-      return undefined;
-    }
-  };
-
-  stack.size = function(){
-    return size;
-  };
+  _.extend(stack, sharedObj);
 
   return stack;
-  
+
 };
