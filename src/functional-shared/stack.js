8a52fbc34eb0d48ed6a258a3a749f30c9bf1e1b6
Hack Reactor Students <github@hackreactor.com>
2013-06-19 15:21:53 -0700

    copy and past two files from src/functional

--- src/functional-shared/stack.js
+++ src/functional-shared/stack.js
@@ -1,3 +1,32 @@
 var makeStack = function(){
-  // Hey! Copy your code from src/functional/stack.js and paste it here
+  // Use an object with numeric keys to store values
+  var storage = {};
+
+  var size = 0;
+
+  // Implement the methods below
+  var stack = {};
+
+  stack.push = function(value){
+    stack[size] = value;
+    size += 1;
+  };
+
+  stack.pop = function(){
+    if(size >= 0) {
+      size -= 1;
+      var temp = stack[size];
+      delete stack[size];
+      return temp;
+    } else {
+      return undefined;
+    }
+  };
+
+  stack.size = function(){
+    return size;
+  };
+
+  return stack;
+  
 };
