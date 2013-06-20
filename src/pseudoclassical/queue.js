05621e03d38515855f8006df154d21c763c9e12c
Hack Reactor Students <github@hackreactor.com>
2013-06-19 19:22:31 -0700

    fix pseudoclassical: queue

--- src/pseudoclassical/queue.js
+++ src/pseudoclassical/queue.js
@@ -1,3 +1,29 @@
 var Queue = function() {
-  // Hey! Copy your code from src/prototypal/queue.js and paste it here
+  this.howBig = 0;
+  this.head_key = 0;
+  this.back_key = 0;
+};
+
+Queue.prototype.enqueue = function(value){
+  this.howBig += 1;
+  this[this.back_key] = value;
+  this.back_key += 1;
+};
+
+Queue.prototype.dequeue = function(){
+  if(this.howBig > 0){
+    this.howBig -= 1;
+
+    var output = this[this.head_key]; //when head_key is 0 and queue is empty, this is undefined
+    delete this[this.head_key];
+
+    this.head_key += 1; //pointing head to the new key
+    return output; //using the old key of the head
+  } else {
+    return undefined;
+  }
+};
+
+Queue.prototype.size = function(){
+  return this.howBig;
 };
