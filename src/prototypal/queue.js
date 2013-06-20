2023a1be4c414d05616c66c5d7805b3fcd2d610c
Hack Reactor Students <github@hackreactor.com>
2013-06-19 19:15:01 -0700

    fix prototypal: queue

--- src/prototypal/queue.js
+++ src/prototypal/queue.js
@@ -1,3 +1,33 @@
 var Queue = function() {
-  // Hey! Copy your code from src/functional-shared/queue.js and paste it here
+  var queue = Object.create(sharedObj);
+  queue.howBig = 0;
+  queue.head_key = 0;
+  queue.back_key = 0;
+  return queue;
+};
+
+var sharedObj = {};
+
+sharedObj.enqueue = function(value){
+  this.howBig += 1;
+  this[this.back_key] = value;
+  this.back_key += 1;
+};
+
+sharedObj.dequeue = function(){
+  if(this.howBig > 0){
+    this.howBig -= 1;
+
+    var output = this[this.head_key]; //when head_key is 0 and queue is empty, this is undefined
+    delete this[this.head_key];
+
+    this.head_key += 1; //pointing head to the new key
+    return output; //using the old key of the head
+  } else {
+    return undefined;
+  }
+};
+
+sharedObj.size = function(){
+  return this.howBig;
 };
