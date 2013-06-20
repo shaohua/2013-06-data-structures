607bae5aa86ae70bff0a355aa4355c0462f7c2fc
Hack Reactor Students <github@hackreactor.com>
2013-06-19 19:12:43 -0700

    completed prototypal: stack

--- src/prototypal/stack.js
+++ src/prototypal/stack.js
@@ -1,3 +1,27 @@
 var Stack = function() {
-  // Hey! Copy your code from src/functional-shared/stack.js and paste it here
+  var stack = Object.create(sharedObj1);
+  stack.howBig = 0;
+  return stack;
+};
+
+var sharedObj1 = {};
+
+sharedObj1.push = function(value){
+  this[this.howBig] = value;
+  this.howBig += 1;
+};
+
+sharedObj1.pop = function(){
+  if(this.howBig >= 0) {
+    this.howBig -= 1;
+    var temp = this[this.howBig];
+    delete this[this.howBig];
+    return temp;
+  } else {
+    return undefined;
+  }
+};
+
+sharedObj1.size = function(){
+  return this.howBig;
 };
