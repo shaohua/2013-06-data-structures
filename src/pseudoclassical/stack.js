c84d7fb89c4fdba324d017e5e88fe90293a79dc9
Hack Reactor Students <github@hackreactor.com>
2013-06-19 19:20:33 -0700

    completed pseudoclassical: stack

--- src/pseudoclassical/stack.js
+++ src/pseudoclassical/stack.js
@@ -1,3 +1,23 @@
-var Queue = function() {
-  // Hey! Copy your code from src/prototypal/stack.js and paste it here
+var Stack = function() {
+  this.howBig = 0;
+};
+
+Stack.prototype.push = function(value){
+  this[this.howBig] = value;
+  this.howBig += 1;
+};
+
+Stack.prototype.pop = function(){
+  if(this.howBig >= 0) {
+    this.howBig -= 1;
+    var temp = this[this.howBig];
+    delete this[this.howBig];
+    return temp;
+  } else {
+    return undefined;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.howBig;
 };
